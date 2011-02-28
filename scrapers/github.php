62b3bf9431552634140b725349b523ab5750c243
roger <leetcoder@gmail.com>
2011-02-27 17:13:19 -0800

    github scraper code complete

--- scrapers/github.php
+++ scrapers/github.php
@@ -4,25 +4,25 @@ require_once './lib/github/Autoloader.php';
 
 $facebook = null;
 $skillKeywords = array();
-$sourceID = 1;
+$sourceID = 3;
 $statusBlob = null;
 setup($facebook, $skillKeywords, $sourceID, $statusBlob);
 
 function buildCoderSourceProfile($sourceID, $siteUserID) {
-	$jsonStr = get_url_contents("http://api.ihackernews.com/profile/$siteUserID");	
-	$userData = json_decode($jsonStr, true);
-	$userName = $userData['username'];
-	$joinDate = siteTimeTextToTimestamp($userData['createdAgo']);
+	global $userData; //hack
+
+	$userName = $userData['defunkt'];
+	$joinDate = strtotime($userData['created_at']);
 	$ranking = 0;
-	$karma = $userData['karma'];
-	$about = $userData['about'];
+	$karma = $userData['followers_count'];
+	$about = $userData['name']." , ".$userData['company']." , ".$userData['location']." , ".$userData['blog']." , ".$userData['email'];
 
 	$coderID = getOrBuildCoderID($userName, $about);
 	doQuery("INSERT INTO codersourceprofiles (coderid, sourceid, username, joindate, ranking, karma, sourcesiteuserid, about) VALUES ($coderID, $sourceID, ".getSQLStrParamStr($userName).", UNIX_TIMESTAMP($joinDate), $ranking, $karma, ".getSQLStrParamStr($siteUserID).", ".getSQLStrParamStr($about).")");		
 	return $coderID;
 }
 
-/*
+
 $userIDs=array();
 for($numFollowers=0; $numFollowers<1800; ++$numFollowers) {
 	echo "\n\nFOLLOWERS = $numFollowers\n\n";
@@ -43,11 +43,25 @@ for($numFollowers=0; $numFollowers<1800; ++$numFollowers) {
 			echo $parts2[0];
 		}
 	}
-}*/
+}
 
-Github_Autoloader::register();
+$userData=array();
+for($i=0; $i<count($userIDs); ++$i) {
+	$userID = $userIDs[$i];
+	$userURL = "http://github.com/api/v2/json/user/show/$userID";
+	$userDataList = json_decode(get_url_contents($userURL));
+	if(is_array($userDataList) && !is_null($userDataList['user'])) {
+		$userData = $userDataList[$user];
+		$likes = floor($userData['followers_count']-3)+$userData['followers_count']*5;
+		$replies = 0;
+		//logNewCoderActivity($sourceID, $skillKeywords, $buildCoderSourceProfileFunc, $siteUserID, $title, $body, $url, $likes, $replies, $ballsRating, $postTime)
+		logNewCoderActivity($sourceID, $skillKeywords, "buildCoderSourceProfile", $userID, "", "", "http://github.com/$userID", $likes, $replies, 0, now());
+	}
+}
+
+/*Github_Autoloader::register();
 $github = new Github_Client();
 $myRepos = $github->getUserApi()->show('ornicar');
-var_dump($myRepos);
+var_dump($myRepos);*/
 
 ?>
